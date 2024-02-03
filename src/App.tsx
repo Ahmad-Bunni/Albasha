7ae92db5a042b04ff54c66f256e627da1cdb7920
DESKTOP-3E4KQ2J\Ahmad <Ahmad-Bunni@hotmail.com>
2024-02-03 01:23:47 +0100

    optimise

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import './App.css';
 import Socials from './socials';
 import backgroundImage from '/images/background.webp';
-
 export default function App() {
+  // Assuming `backgroundImage` is imported or defined elsewhere in your code.
   return (
-    <div className="relative flex flex-col justify-between h-screen">
+    <div className="relative flex flex-col justify-between min-h-screen">
       <div className="flex flex-col space-y-8">
-        <div className="self-center overflow-hidden rounded-md shadow-md">
+        <div className="self-center overflow-hidden rounded-md shadow-md max-w-full mx-auto">
           <img
-            alt="Chicken Shawarma at Al-Basha Market & Grill"
-            className="h-auto object-cover"
             src={backgroundImage}
-            loading="lazy"
-            width={720}
+            alt="Chicken Shawarma at Al-Basha Market & Grill"
+            className="w-full h-auto md:w-[720px] md:h-[480px] object-cover"
           />
         </div>
 
-        <div className="text-center text-zinc-700">
-          <span className="font-bold block text-5xl ">Now Open!</span>
-          <span className="font-bold block text-xl mt-4">
+        <div className="text-center text-zinc-700 px-4">
+          <span className="font-bold block text-3xl md:text-5xl ">
+            Now Open!
+          </span>
+          <span className="font-bold block text-lg md:text-xl mt-2 md:mt-4">
             Online Ordering Available Soon
           </span>
         </div>
@@ -26,7 +26,7 @@ export default function App() {
         <Socials />
       </div>
 
-      <footer className="bg-red-700 text-white text-center p-4 lg:text-xl text-sm">
+      <footer className="bg-red-700 text-white text-center p-4 text-sm md:lg:text-xl">
         Copyright © {new Date().getFullYear()} Albasha - All Rights Reserved
       </footer>
     </div>
